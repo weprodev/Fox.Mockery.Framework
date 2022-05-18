7adc7d8df4020c711d570860badbf9ee8d412a69
Michael Andish <maikel1370@gmail.com>
2022-05-18 15:27:17 +0200

    feat: creating a default method for base url without service name

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function default(Request $request): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Please add the service name as a prefix in the url.',
+            'example' => '{domain}/service_name/*',
+            'services' => [
+                'description' => 'You can manage your services in config directory.',
+                'available' => getAvailableServices(),
+                'all' => getAllServices(),
+            ],
+            'data' => [
+                'method' => $request->method(),
+                'request' => $request->all(),
+                'header' => $request->header(),
+            ],
+        ]);
+    }
 }
